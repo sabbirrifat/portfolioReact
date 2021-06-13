7b6aa29fffbdfa85e770a4a9b70da798f93ce961
Sabbir Rifat <rifat234dgh@gmail.com>
2021-06-13 08:30:46 +0600

    Almost done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,27 @@
-import React, {useEffect, useState} from 'react'
-import {Link} from 'react-scroll';
-import {FaChevronUp} from 'react-icons/fa'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-scroll";
+import { FaChevronUp } from "react-icons/fa";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
-
-const Navbar = () => {
+const NavbarMain = () => {
 
   const [handleShow, sethandleShow] = useState(false);
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if(window.scrollY > 150){
-                sethandleShow(true)
-            }
-            else{
-                sethandleShow(false)
-            }
-        });
-        return () => {
-            window.removeEventListener("scroll");
-        }
-        
-    }, [])
-    return (<>
-
-        <nav
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 150) {
+        sethandleShow(true);
+      } else {
+        sethandleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <>
+      {/* <nav
           className={`navbar navbar-b ${handleShow ? 'navbar-reduce' : 'navbar-trans'} navbar-expand-md fixed-top`}
           id="mainNav"
           style={{ padding: '25px'}}
@@ -65,10 +64,54 @@ const Navbar = () => {
               </ul>
             </div>
           </div>
-        </nav>
-        <Link to="home" class="back-to-top" smooth={true} style={{ display: handleShow ? 'inline' : 'none'}}><FaChevronUp /></Link>
-        </>
-    )
-}
+        </nav> */}
 
-export default Navbar
+      <Navbar
+        collapseOnSelect
+        expand="md"
+        fixed="top"
+        className={`navbar navbar-b ${handleShow ? 'navbar-reduce' : 'navbar-trans'}`}
+      >
+        <div className="container" style={{ paddingTop: '8px'}}>
+          <Navbar.Brand href="#" style={{ fontSize: '26px', color: '#fff'}}>Sabbir Rifat</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick={() => !handleShow ? sethandleShow(true) : null }>
+              <span></span>
+              <span></span>
+              <span></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <ul class="navbar-nav ml-auto">
+            
+              <li class="nav-item">
+                  <Link class="nav-link js-scroll active" to="home" smooth={true} >Home</Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link js-scroll" to="about" smooth={true} >About</Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link js-scroll" to="service" smooth={true} >Skills</Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link js-scroll" to="work" smooth={true} >Projects</Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link js-scroll" to="contact" smooth={true} >Contact</Link>
+                </li>
+            </ul>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+
+      <Link
+        to="home"
+        class="back-to-top"
+        smooth={true}
+        style={{ display: handleShow ? "inline" : "none" }}
+      >
+        <FaChevronUp />
+      </Link>
+    </>
+  );
+};
+
+export default NavbarMain;
