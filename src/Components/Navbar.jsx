d2dc1c60d729b55784a3e4e6043b48c48ff2a774
Sabbir Rifat <rifat234dgh@gmail.com>
2021-06-13 08:31:20 +0600

    Navbar problem fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -21,51 +21,6 @@ const NavbarMain = () => {
   }, []);
   return (
     <>
-      {/* <nav
-          className={`navbar navbar-b ${handleShow ? 'navbar-reduce' : 'navbar-trans'} navbar-expand-md fixed-top`}
-          id="mainNav"
-          style={{ padding: '25px'}}
-        >
-          <div class="container">
-            <Link class="navbar-brand js-scroll" to="home" smooth={true} >Sabbir Rifat</Link>
-            <button
-              class="navbar-toggler collapsed"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarDefault"
-              aria-controls="navbarDefault"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span></span>
-              <span></span>
-              <span></span>
-            </button>
-            <div
-              class="navbar-collapse collapse justify-content-end"
-              id="navbarDefault"
-            >
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <Link class="nav-link js-scroll active" to="home" smooth={true} >Home</Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link js-scroll" to="about" smooth={true} >About</Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link js-scroll" to="service" smooth={true} >Skills</Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link js-scroll" to="work" smooth={true} >Projects</Link>
-                </li>
-                <li class="nav-item">
-                  <Link class="nav-link js-scroll" to="contact" smooth={true} >Contact</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav> */}
-
       <Navbar
         collapseOnSelect
         expand="md"
